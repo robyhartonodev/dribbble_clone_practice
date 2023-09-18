ce4434ef9e5694eacb0879fa8823f6863ae87ccf
roby <roby.hartono@empulse.de>
2023-09-18 16:09:03 +0200

    Implementing tab bar view

--- lib/main.dart
+++ lib/main.dart
@@ -47,50 +47,53 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: <Widget>[
-        DetailPage(
-          foodItem: FoodItem(
-            ratings: 4.0,
-            name: 'Hot Coffee',
-            type: 'Coffee',
-            icon: null,
-            description: 'Testing',
-            price: 15.0,
-            image: 'images/coffee.png',
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        body: <Widget>[
+          DetailPage(
+            foodItem: FoodItem(
+              ratings: 4.0,
+              name: 'Hot Coffee',
+              type: 'Coffee',
+              icon: null,
+              description: 'Testing',
+              price: 15.0,
+              image: 'images/coffee.png',
+            ),
           ),
-        ),
-        DetailPage(
-          foodItem: FoodItem(
-            ratings: 4.0,
-            name: 'Hot Coffee',
-            type: 'Coffee',
-            icon: null,
-            description: 'Testing',
-            price: 15.0,
-            image: 'images/coffee.png',
+          DetailPage(
+            foodItem: FoodItem(
+              ratings: 4.0,
+              name: 'Hot Coffee',
+              type: 'Coffee',
+              icon: null,
+              description: 'Testing',
+              price: 15.0,
+              image: 'images/coffee.png',
+            ),
           ),
+          HomePage(),
+          HomePage(),
+          HomePage(),
+        ][currentPageIndex],
+        bottomNavigationBar: NavigationBar(
+          onDestinationSelected: (int index) {
+            setState(() {
+              currentPageIndex = index;
+            });
+          },
+          selectedIndex: currentPageIndex,
+          destinations: const <Widget>[
+            NavigationDestination(icon: Icon(Icons.home_filled), label: 'Home'),
+            NavigationDestination(icon: Icon(Icons.coffee), label: 'Order'),
+            NavigationDestination(icon: Icon(Icons.search), label: 'Search'),
+            NavigationDestination(
+                icon: Icon(Icons.workspace_premium), label: 'Reward'),
+            NavigationDestination(icon: Icon(Icons.list), label: 'Menu')
+          ],
+          surfaceTintColor: Colors.white,
         ),
-        HomePage(),
-        HomePage(),
-        HomePage(),
-      ][currentPageIndex],
-      bottomNavigationBar: NavigationBar(
-        onDestinationSelected: (int index) {
-          setState(() {
-            currentPageIndex = index;
-          });
-        },
-        selectedIndex: currentPageIndex,
-        destinations: const <Widget>[
-          NavigationDestination(icon: Icon(Icons.home_filled), label: 'Home'),
-          NavigationDestination(icon: Icon(Icons.coffee), label: 'Order'),
-          NavigationDestination(icon: Icon(Icons.search), label: 'Search'),
-          NavigationDestination(
-              icon: Icon(Icons.workspace_premium), label: 'Reward'),
-          NavigationDestination(icon: Icon(Icons.list), label: 'Menu')
-        ],
-        surfaceTintColor: Colors.white,
       ),
     );
   }
--- lib/pages/detail_page.dart
+++ lib/pages/detail_page.dart
@@ -28,138 +28,144 @@ class _DetailPageState extends State<DetailPage> with TickerProviderStateMixin {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Column(
-        children: [
-          Stack(
-            children: [
-              Container(
-                decoration: BoxDecoration(boxShadow: [
-                  BoxShadow(
-                    color: Colors.black.withOpacity(0.2),
-                    spreadRadius: 5,
-                    blurRadius: 10,
-                  )
-                ]),
-                child: ClipRRect(
-                  borderRadius: const BorderRadius.only(
-                    bottomLeft: Radius.circular(32.0),
-                    bottomRight: Radius.circular(32.0),
-                  ),
-                  child: Image(
-                    width: double.infinity,
-                    height: 430,
-                    image: AssetImage(widget.foodItem.image),
-                    fit: BoxFit.cover,
-                  ),
-                ),
-              ),
-              Positioned(
-                top: 48,
-                child: Container(
-                  padding: const EdgeInsets.only(right: 8.0, left: 8.0),
-                  width: MediaQuery.of(context).size.width,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      IconButton(
-                        onPressed: () {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => MyHomePage()));
-                        },
-                        icon: const Icon(
-                          Icons.arrow_back,
-                          color: Colors.grey,
-                          size: 24,
-                        ),
-                      ),
-                      IconButton(
-                        onPressed: () {},
-                        icon: const Icon(
-                          Icons.file_upload_outlined,
-                          color: Colors.grey,
-                          size: 24,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ],
-          ),
-          Container(
-            padding: EdgeInsets.all(32.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.start,
+    return ListView(
+      children: [
+        Column(
+          children: [
+            Stack(
               children: [
-                Text(
-                  "Hot Coffee",
-                  style: TextStyle(
-                    fontSize: 24.0,
-                    fontWeight: FontWeight.bold,
+                Container(
+                  decoration: BoxDecoration(boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.2),
+                      spreadRadius: 5,
+                      blurRadius: 10,
+                    )
+                  ]),
+                  child: ClipRRect(
+                    borderRadius: const BorderRadius.only(
+                      bottomLeft: Radius.circular(32.0),
+                      bottomRight: Radius.circular(32.0),
+                    ),
+                    child: Image(
+                      width: double.infinity,
+                      height: 430,
+                      image: AssetImage(widget.foodItem.image),
+                      fit: BoxFit.cover,
+                    ),
                   ),
                 ),
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    Text(
-                      "\$15",
-                      style: TextStyle(
-                        fontSize: 24.0,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
+                Positioned(
+                  top: 48,
+                  child: Container(
+                    padding: const EdgeInsets.only(right: 8.0, left: 8.0),
+                    width: MediaQuery.of(context).size.width,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
-                        Icon(
-                          Icons.location_on,
-                          color: Colors.grey,
+                        IconButton(
+                          onPressed: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => MyHomePage()));
+                          },
+                          icon: const Icon(
+                            Icons.arrow_back,
+                            color: Colors.grey,
+                            size: 24,
+                          ),
                         ),
-                        Text(
-                          "20 min",
-                          style: TextStyle(color: Colors.grey),
-                        )
+                        IconButton(
+                          onPressed: () {},
+                          icon: const Icon(
+                            Icons.file_upload_outlined,
+                            color: Colors.grey,
+                            size: 24,
+                          ),
+                        ),
                       ],
                     ),
-                  ],
+                  ),
                 ),
               ],
             ),
-          ),
-          Column(
-            children: [
-              Container(
-                padding: EdgeInsets.all(8.0),
-                child: TabBar(
-                  controller: _tabController,
-                  tabs: [
-                    Tab(text: 'Tab 1'),
-                    Tab(text: 'Tab 2'),
-                    Tab(text: 'Tab 3'),
-                  ],
-                ),
+            Container(
+              padding: const EdgeInsets.all(32.0),
+              child: const Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Hot Coffee",
+                    style: TextStyle(
+                      fontSize: 24.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Text(
+                        "\$15",
+                        style: TextStyle(
+                          fontSize: 24.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Icon(
+                            Icons.location_on,
+                            color: Colors.grey,
+                          ),
+                          Text(
+                            "20 min",
+                            style: TextStyle(color: Colors.grey),
+                          )
+                        ],
+                      ),
+                    ],
+                  ),
+                ],
               ),
-              // TabBarView(
-              //   controller: _tabController,
-              //   children: [
-              //     // Content of Tab 1
-              //     Center(child: Text('Tab 1 Content')),
-              //
-              //     // Content of Tab 2
-              //     Center(child: Text('Tab 2 Content')),
-              //
-              //     // Content of Tab 3
-              //     Center(child: Text('Tab 3 Content')),
-              //   ],
-              // ),
-            ],
-          )
-        ],
-      ),
+            ),
+            Column(
+              children: [
+                Container(
+                  padding: EdgeInsets.all(8.0),
+                  child: TabBar(
+                    controller: _tabController,
+                    tabs: const [
+                      Tab(text: 'Tab 1'),
+                      Tab(text: 'Tab 2'),
+                      Tab(text: 'Tab 3'),
+                    ],
+                  ),
+                ),
+                // TabBarView(
+                //   controller: _tabController,
+                //   children: [
+                //     // Content of Tab 1
+                //     Container(
+                //       child: Text('Test 1'),
+                //     ),
+                //     // Content of Tab 2
+                //     Container(
+                //       child: Text('Test 2'),
+                //     ),
+                //     // Content of Tab 3
+                //     Container(
+                //       child: Text('Test 3'),
+                //     ),
+                //   ],
+                // ),
+              ],
+            )
+          ],
+        ),
+      ],
     );
   }
 }
